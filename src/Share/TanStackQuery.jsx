9fcce9a4193a2dc8718483165aa7bff9d701a0c5
TanvirAhmed-1 <tanvirahmed1078@gmail.com>
2025-04-14 00:38:11 +0600

    email base user cart fetch

--- src/Share/TanStackQuery.jsx
+++ src/Share/TanStackQuery.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "./useAxios";
+import { useContext } from "react";
+import { AuthContext } from "../components/Authountation/Authorization";
 
 
 const TanStackQuery = () => {
+    const{users}=useContext(AuthContext)
     const axiosBaseUrl=useAxios()
     const {data,refetch}=useQuery({
-        queryKey:["cart"],
+        queryKey:["cart",users?.email],
         queryFn: async ()=>{
-            const res=await axiosBaseUrl.get("/orders")
+            const res=await axiosBaseUrl.get(`/orders?email=${users.email}`)
             return res.data
         }
     })
